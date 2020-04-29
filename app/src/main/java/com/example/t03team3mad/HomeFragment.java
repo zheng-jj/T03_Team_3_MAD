ab4addf8ad44e3650f83f0a2206fbe83f1638e83
zheng-jj <64000939+zheng-jj@users.noreply.github.com>
2020-04-29 10:53:53 +0800

    Home fragment created and linked to main activity

---
+++ app/src/main/java/com/example/t03team3mad/HomeFragment.java
@@ -0,0 +1,20 @@
+package com.example.t03team3mad;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+    private static final String TAG = "HomeFragment";
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/t03team3mad/MainActivity.java
+++ app/src/main/java/com/example/t03team3mad/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.t03team3mad;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        starthomefragment();
+    }
+    private void starthomefragment(){
+        HomeFragment homeFragment = new HomeFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.mainactivitycontainer,homeFragment,"HomeFragment");
+        transaction.addToBackStack("HomeFragment");
+        transaction.commit();
     }
 }
