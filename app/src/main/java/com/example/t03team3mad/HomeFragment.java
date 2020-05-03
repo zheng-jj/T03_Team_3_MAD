40c0e56a061f52319d45df6cc2b8095e614d407e
zheng-jj <64000939+zheng-jj@users.noreply.github.com>
2020-05-03 18:43:51 +0800

    Scroll view for container fixed

--- app/src/main/java/com/example/t03team3mad/HomeFragment.java
+++ app/src/main/java/com/example/t03team3mad/HomeFragment.java
@@ -4,17 +4,25 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HomeFragment extends Fragment {
     private static final String TAG = "HomeFragment";
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home,container,false);
+        Button follow = view.findViewById(R.id.follow1);
+        follow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG,"Follow button clicked");
+            }
+        });
         return view;
     }
+
 }
