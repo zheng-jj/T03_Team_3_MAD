030fb24755b9554e3f76015de60ea03cbca9f076
Jian Oon Swah <s10195746@connect.np.edu.sg>
2020-05-05 16:32:37 +0800

    Added 2 java class for database access - constructor to extract data from database using sql Added example + guide on how to access database using sql

---
+++ app/src/main/java/com/example/t03team3mad/DatabaseAccess.java
@@ -0,0 +1,43 @@
+package com.example.t03team3mad;
+import java.util.HashMap;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseAccess {
+    private SQLiteOpenHelper openHelper;
+    private SQLiteDatabase db;
+    private static DatabaseAccess instance;
+    private Cursor temp = null;
+    String output;
+    private DatabaseAccess (Context context){
+        this.openHelper = new DatabaseOpenHelper(context);
+
+    }
+    public static DatabaseAccess getInstance(Context context){
+        if(instance == null){
+            instance = new DatabaseAccess(context);
+        }
+        return instance;
+    }
+    public void open(){
+        this.db = openHelper.getWritableDatabase();
+
+    }
+    public void close(){
+        if(db!=null){
+            this.db.close();
+        }
+    }
+    public String getElement(String column,String Table,String condition_column,String condition){
+        temp = db.rawQuery ("select " + column +" from " + Table +" where " + condition_column + " = '"+ condition + "'",new  String[]{} );
+        while(temp.moveToNext()){
+            output = temp.getString(0);
+        }
+        return output;
+    }
+
+
+
+}
--- app/src/main/java/com/example/t03team3mad/authorprofileFragment.java
+++ app/src/main/java/com/example/t03team3mad/authorprofileFragment.java
@@ -5,14 +5,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class authorprofileFragment extends Fragment {
+
     private static final String TAG = "authorprofileFragment";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_authorprofile,container,false);
+        TextView name = view.findViewById(R.id.profilename);
+
+        // EXAMPLE TO EXTRACT INFORMATION
+        // SELECT (column) FROM (table) WHERE (condition_column) = (condition)
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(getActivity().getApplicationContext());
+        databaseAccess.open();
+        name.setText(databaseAccess.getElement("NAME","Author","Name","J. K. Rowling"));
+        databaseAccess.close();
+
         return view;
     }
 }
