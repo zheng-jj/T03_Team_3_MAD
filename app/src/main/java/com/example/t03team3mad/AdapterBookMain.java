d45229efdec74b1e2fe2505117ffa1887579bda0
zheng-jj <64000939+zheng-jj@users.noreply.github.com>
2020-05-06 09:23:44 +0800

    Commented out adapters

--- app/src/main/java/com/example/t03team3mad/AdapterBookMain.java
+++ app/src/main/java/com/example/t03team3mad/AdapterBookMain.java
@@ -1,39 +1,39 @@
-package com.example.t03team3mad;
-
-import android.content.Context;
-import android.widget.Adapter;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import android.view.LayoutInflater;
-import java.util.List;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-public class AdapterBookMain extends RecyclerView.Adapter<AdapterBookMain.ViewHolder>
-{
-    List<Book> mBooklist = new List<Book>(){};
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(View itemView) {
-            super(itemView);
-        }
-    }
-    public AdapterBookMain(List<Book> mBooklist) {
-    }
-    @Override
-    public AdapterBookMain.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View contactView = inflater.inflate(R.layout.bookdisplaycardview, parent, false);
-        ViewHolder viewHolder = new ViewHolder(contactView);
-        return viewHolder;
-    }
-    @Override
-    public void onBindViewHolder(AdapterBookMain.ViewHolder viewHolder, int position) {
-    }
-    @Override
-    public int getItemCount() {
-        return mBooklist.size();
-    }
-}
+//package com.example.t03team3mad;
+//
+//import android.content.Context;
+//import android.widget.Adapter;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.TextView;
+//import android.view.LayoutInflater;
+//import java.util.List;
+//
+//import androidx.annotation.NonNull;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//public class AdapterBookMain extends RecyclerView.Adapter<AdapterBookMain.ViewHolder>
+//{
+//    List<Book> mBooklist = new List<Book>(){};
+//    public class ViewHolder extends RecyclerView.ViewHolder {
+//        public ViewHolder(View itemView) {
+//            super(itemView);
+//        }
+//    }
+//    public AdapterBookMain(List<Book> mBooklist) {
+//    }
+//    @Override
+//    public AdapterBookMain.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        Context context = parent.getContext();
+//        LayoutInflater inflater = LayoutInflater.from(context);
+//        View contactView = inflater.inflate(R.layout.bookdisplaycardview, parent, false);
+//        ViewHolder viewHolder = new ViewHolder(contactView);
+//        return viewHolder;
+//    }
+//    @Override
+//    public void onBindViewHolder(AdapterBookMain.ViewHolder viewHolder, int position) {
+//    }
+//    @Override
+//    public int getItemCount() {
+//        return mBooklist.size();
+//    }
+//}
--- app/src/main/java/com/example/t03team3mad/AdapterUserMain.java
+++ app/src/main/java/com/example/t03team3mad/AdapterUserMain.java
@@ -1,40 +1,40 @@
-package com.example.t03team3mad;
-
-import android.content.Context;
-import android.widget.Adapter;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import android.view.LayoutInflater;
-import java.util.List;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-public class AdapterUserMain extends RecyclerView.Adapter<AdapterUserMain.ViewHolder>
-{
-    List<User> mUserlist = new List<User>(){};
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(View itemView) {
-            super(itemView);
-        }
-    }
-    public AdapterUserMain(List<User> mUserlist) {
-    }
-    @Override
-    public AdapterUserMain.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
-        LayoutInflater inflater = LayoutInflater.from(context);
-        //Complete the card view for users
-        //View contactView = inflater.inflate(R.layout.Cardviewuser, parent, false);
-        ViewHolder viewHolder = new ViewHolder(contactView);
-        return viewHolder;
-    }
-    @Override
-    public void onBindViewHolder(AdapterUserMain.ViewHolder viewHolder, int position) {
-    }
-    @Override
-    public int getItemCount() {
-        return mUserlist.size();
-    }
-}
+//package com.example.t03team3mad;
+//
+//import android.content.Context;
+//import android.widget.Adapter;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.TextView;
+//import android.view.LayoutInflater;
+//import java.util.List;
+//
+//import androidx.annotation.NonNull;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//public class AdapterUserMain extends RecyclerView.Adapter<AdapterUserMain.ViewHolder>
+//{
+//    List<User> mUserlist = new List<User>(){};
+//    public class ViewHolder extends RecyclerView.ViewHolder {
+//        public ViewHolder(View itemView) {
+//            super(itemView);
+//        }
+//    }
+//    public AdapterUserMain(List<User> mUserlist) {
+//    }
+//    @Override
+//    public AdapterUserMain.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        Context context = parent.getContext();
+//        LayoutInflater inflater = LayoutInflater.from(context);
+//        //Complete the card view for users
+//        //View contactView = inflater.inflate(R.layout.Cardviewuser, parent, false);
+//        ViewHolder viewHolder = new ViewHolder(contactView);
+//        return viewHolder;
+//    }
+//    @Override
+//    public void onBindViewHolder(AdapterUserMain.ViewHolder viewHolder, int position) {
+//    }
+//    @Override
+//    public int getItemCount() {
+//        return mUserlist.size();
+//    }
+//}
