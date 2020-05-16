a0eec5cee77a679ec5f949871d134ed6e99b05f9
zheng-jj <64000939+zheng-jj@users.noreply.github.com>
2020-05-17 04:13:46 +0800

    Book cardview adapter complete. Book Recyclerview implemented for mainpage. Added function to load books from database fixed some errors

--- app/src/main/java/com/example/t03team3mad/DatabaseAccess.java
+++ app/src/main/java/com/example/t03team3mad/DatabaseAccess.java
@@ -69,4 +69,42 @@ public class DatabaseAccess {
             }while (cursor.moveToNext());
         return  mUserlist;
     }
+    //jj- same as for load userlist
+    public List<Book> loadallbooklist() {
+        List<Book> mBooklist = new ArrayList<Book>(){};
+        Integer amountofusers =Integer.valueOf(getCount("IDU","USER"));
+        cursor = db.rawQuery("Select * From Book",new  String[]{});
+        cursor.moveToFirst();
+        do {
+            String title = cursor.getString(cursor.getColumnIndex("TITLE"));
+            String author = cursor.getString(cursor.getColumnIndex("AUTHOR"));
+            String about = cursor.getString(cursor.getColumnIndex("ABOUT"));
+            String genre = cursor.getString(cursor.getColumnIndex("GENRE"));
+            String pdate = cursor.getString(cursor.getColumnIndex("PDATE"));
+            String isbn = cursor.getString(cursor.getColumnIndex("ISBN"));
+            Book booktoaddtolist = new Book(title,author,about,genre,pdate,isbn);
+            mBooklist.add(booktoaddtolist);
+        }while (cursor.moveToNext());
+        return  mBooklist;
+    }
+
+    //qh - method to search books in the database
+    public List<Book> searchbook(String query) {
+        List<Book> booklist = new ArrayList<Book>();
+        Cursor c = db.rawQuery("SELECT * FROM BOOK WHERE TRIM(TITLE) LIKE '%" + query + "%'", new String[]{});
+        if (c.moveToFirst() && c.getCount() >= 1) {
+            do {
+                String title = c.getString(0);
+                String ida = c.getString(1);
+                String about = c.getString(2);
+                String genre = c.getString(3);
+                String pdate = c.getString(4);
+                String isbn = c.getString(5);
+                Book book1 = new Book(title, ida, about, genre, pdate, isbn);
+                booklist.add(book1);
+                System.out.println(book1.getBooktitle());
+            } while (c.moveToNext());
+        }
+        return booklist;
+    }
 }
