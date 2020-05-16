88b651fbbb02e058e91c3323f473323da2dd8fde
zheng-jj <64000939+zheng-jj@users.noreply.github.com>
2020-05-17 04:15:10 +0800

    Book cardview adapter complete. Book Recyclerview implemented for mainpage. Added function to load books from database fixed some errors 2

--- app/src/main/java/com/example/t03team3mad/AdapterBookMain.java
+++ app/src/main/java/com/example/t03team3mad/AdapterBookMain.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.widget.Adapter;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.view.LayoutInflater;
 
@@ -11,19 +12,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.t03team3mad.model.Book;
-
+//jj- all the same format as adapterusermain
 public class AdapterBookMain extends RecyclerView.Adapter<AdapterBookMain.ViewHolder>
 {
     List<Book> mBooklist = new ArrayList<Book>(){};
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(View itemView) {
+        CardView cardView;
+        TextView bookName;
+        ImageView bookPic;
+        ViewHolder(View itemView) {
             super(itemView);
+            cardView = (CardView)itemView.findViewById(R.id.bookCardView);
+            //jj-get the widget id and assign them to local variable
+            bookName = (TextView)itemView.findViewById(R.id.bookname);
+            bookPic = (ImageView)itemView.findViewById(R.id.bookimage);
         }
     }
     public AdapterBookMain(List<Book> mBooklist) {
+        this.mBooklist = mBooklist;
     }
     @Override
     public AdapterBookMain.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -35,9 +45,15 @@ public class AdapterBookMain extends RecyclerView.Adapter<AdapterBookMain.ViewHo
     }
     @Override
     public void onBindViewHolder(AdapterBookMain.ViewHolder viewHolder, int position) {
+        viewHolder.bookName.setText(mBooklist.get(position).getBooktitle());
+        //jj-this needs to change to the corrosponding user profile picture
+        viewHolder.bookPic.setImageResource(R.drawable.demo_book_pic);
     }
     @Override
     public int getItemCount() {
         return mBooklist.size();
+    }
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
     }
 }
--- app/src/main/java/com/example/t03team3mad/HomeFragment.java
+++ app/src/main/java/com/example/t03team3mad/HomeFragment.java
@@ -3,6 +3,8 @@ package com.example.t03team3mad;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,11 +12,47 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.t03team3mad.model.Book;
+import com.example.t03team3mad.model.User;
+
+import java.util.List;
+
 public class HomeFragment extends Fragment {
     private static final String TAG = "HomeFragment";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home,container,false);
+
+        //load main popularbooks recyclerview
+        RecyclerView popularbooks = (RecyclerView)view.findViewById(R.id.popularbookrecyclerview);
+        //jj-layout manager linear layout manager manages the position of the recyclerview items
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        //jj-set the recyclerview's manager to the previously created manager
+        popularbooks.setLayoutManager(llm);
+        //jj- get the data needed by the adapter to fill the cardview and put it in the adapter's parameters
+        AdapterBookMain bookadapter  = new AdapterBookMain(loadAllBooks());
+        //jj- set the recyclerview object to its adapter
+        popularbooks.setAdapter(bookadapter);
+
+        //load recommended books recyclerview
+        //do the same for another recycler view recommendedbooks
+        RecyclerView recommended = (RecyclerView)view.findViewById(R.id.recommendbookrecyclerview);
+        LinearLayoutManager llm2 = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        //jj-set the recyclerview's manager to the previously created manager
+        recommended.setLayoutManager(llm2);
+        //jj- get the data needed by the adapter to fill the cardview and put it in the adapter's parameters
+        AdapterBookMain bookadapter2  = new AdapterBookMain(loadAllBooks());
+        //jj- set the recyclerview object to its adapter
+        recommended.setAdapter(bookadapter2);
         return view;
+    }
+    //load all books into a list
+    public List<Book> loadAllBooks()
+    {
+        DatabaseAccess DBaccess = DatabaseAccess.getInstance(getActivity().getApplicationContext());
+        DBaccess.open();
+        List<Book> mBooklist = DBaccess.loadallbooklist();
+        DBaccess.close();
+        return mBooklist;
     }
 }
