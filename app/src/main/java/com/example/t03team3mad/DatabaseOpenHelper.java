129f1baf4400366a4b6b664e3451c1d657b99c49
Jian Oon Swah <s10195746@connect.np.edu.sg>
2020-06-01 14:21:49 +0800

    change db name in openhelper

--- app/src/main/java/com/example/t03team3mad/DatabaseOpenHelper.java
+++ app/src/main/java/com/example/t03team3mad/DatabaseOpenHelper.java
@@ -8,7 +8,7 @@ import com.example.t03team3mad.model.Book;
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class DatabaseOpenHelper extends SQLiteAssetHelper {
-    private static final String DATABASE_NAME="finalandroid.db";
+    private static final String DATABASE_NAME="finaldb.db";
     private static final int DATABASE_VERSION=1;
     public DatabaseOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
