6995f7ed16f209b1c4237ba469ff225bc83f67b1
zheng-jj <64000939+zheng-jj@users.noreply.github.com>
2020-07-07 13:44:12 +0800

    api for multiple books at once

---
+++ app/src/main/java/com/example/t03team3mad/APIaccessBookList.java
@@ -0,0 +1,93 @@
+package com.example.t03team3mad;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.example.t03team3mad.model.Book;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class APIaccessBookList  extends AsyncTask<String,Void, ArrayList<Book>> {
+    //jj- api url
+    private String apiurl = "https://openlibrary.org/";
+    private static final String TAG = "APIaccess";
+    //jj-empty contructor
+    public APIaccessBookList(){}
+    //jj - search book by isbn using API
+    public ArrayList<Book> searchbookbyisbn(String isbn) throws IOException, JSONException {
+        ArrayList<Book> listofresults = new ArrayList<>();
+        String[] isbns = isbn.split(";");
+        for(String isbntosearch:isbns ){
+            Log.v(TAG,"Searching api isbn ="+isbntosearch);
+            //jj-sets the url to GET data as json
+            URL url = new URL(apiurl+"api/books?bibkeys=ISBN:"+isbntosearch+"&jscmd=details&format=json");
+            //jj-opens the connection
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("GET");
+            int responsecode = conn.getResponseCode();
+            //jj-checks if the response is successful
+            if(responsecode != 200)
+                throw new RuntimeException("HttpResponseCode: " +responsecode);
+            else {
+                //jj - gets the string from the url
+                Scanner sc = new Scanner(url.openStream());
+                JSONObject bookjsonobj = null;
+                while (sc.hasNext()) {
+                    String temps = sc.nextLine();
+                    //jj-converts string into json object to use
+                    bookjsonobj = new JSONObject(temps);
+                    Log.v(TAG, "json created here " + bookjsonobj);
+                    //jj- i only need 1 record, hence break after 1 loop
+                    break;
+                }
+                sc.close();
+                //jj - use this get data from json object to parse into object
+                if (bookjsonobj != null) {
+                    String booktitle = bookjsonobj.getJSONObject("ISBN:" + isbn).getJSONObject("details").getString("title");
+                    String bookauthor = bookjsonobj.getJSONObject("ISBN:" + isbn).getJSONObject("details").getJSONArray("authors").getJSONObject(0).getString("name");
+                    String genrelist = "";
+                    JSONArray subjects = bookjsonobj.getJSONObject("ISBN:" + isbn).getJSONObject("details").getJSONArray("subjects");
+                    //jj-loops through all the subjects in the list of subjects and adds to a string
+                    for (int i = 0; i < subjects.length(); i++) {
+                        genrelist = genrelist + subjects.getString(i) + ";";
+                    }
+                    //jj-removes the last ";" at the end of list of genres
+                    String bookgenre = genrelist.substring(0, genrelist.length() - 1);
+                    String bookdes = bookjsonobj.getJSONObject("ISBN:" + isbn).getJSONObject("details").getString("description");
+                    String bookpdate = bookjsonobj.getJSONObject("ISBN:" + isbn).getJSONObject("details").getString("publish_date");
+                    //jj-creates the book object with json data
+                    Book x = new Book(booktitle, bookauthor, bookdes, bookgenre, bookpdate, isbn);
+                    listofresults.add(x);
+                }
+            }
+        }
+        return listofresults;
+    }
+    private Exception exception;
+
+    //jj-method that calls the searchbook method and runs it in background
+    @Override
+    protected ArrayList<Book> doInBackground(String... urls) {
+        try {
+            Log.v(TAG, urls[0]);
+            Log.v(TAG, "TestLOG");
+            return searchbookbyisbn(urls[0]);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+    protected void onPostExecute(ArrayList<Book> books) {
+        Log.v(TAG,"task executed");
+    }
+}
