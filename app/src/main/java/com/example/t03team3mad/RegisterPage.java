18981065c565dd383d17ecff3117897956da75f2
Weixian Christopher Tey <s10193226@connect.np.edu.sg>
2020-07-08 15:53:30 +0800

    registering can be stored in firestore properly.

--- app/src/main/java/com/example/t03team3mad/RegisterPage.java
+++ app/src/main/java/com/example/t03team3mad/RegisterPage.java
@@ -13,6 +13,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 import com.example.t03team3mad.model.Member;
+import com.example.t03team3mad.model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -22,9 +23,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterPage extends AppCompatActivity {
     EditText EnterEmail,EnterPassword,EnterName,ConfirmPassword;
@@ -32,6 +39,7 @@ public class RegisterPage extends AppCompatActivity {
     FirebaseAuth Auth;
     DatabaseReference databaseReference;
     Member member;
+    User user;
     ProgressBar progressBar;
     long maxid=0;
     private static final String TAG = "RegisterPage";
@@ -68,53 +76,50 @@ public class RegisterPage extends AppCompatActivity {
             @SuppressLint("ShowToast")
             @Override
             public void onClick(View v) {
-                String email, password,name,confirmPassword;
+                String email, password, name, confirmPassword;
                 email = EnterEmail.getText().toString();
                 password = EnterPassword.getText().toString();
                 name = EnterName.getText().toString();
                 confirmPassword = ConfirmPassword.getText().toString();
                 //Chris - Verification for inputs
                 //Chris - Check for empty Inputs
-                if (name.equals(""))
-                {
-                    Log.v(TAG,"Name Required");
+                if (name.equals("")) {
+                    Log.v(TAG, "Name Required");
                     Toast.makeText(RegisterPage.this, "Name Required", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if (email.equals(""))
-                {
-                    Log.v(TAG,"Email Required");//Chris - Check for empty Inputs
+                if (email.equals("")) {
+                    Log.v(TAG, "Email Required");//Chris - Check for empty Inputs
                     Toast.makeText(RegisterPage.this, "Email Required", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 if (password.equals(""))//Chris - Check for empty Inputs
                 {
-                    Log.v(TAG,"Password Required");
+                    Log.v(TAG, "Password Required");
                     Toast.makeText(RegisterPage.this, "Password Required", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 if (password.length() < 6)//Chris - to check password hit the minimal characters of the password requirement
                 {
-                    Log.v(TAG,"Password is must be at least contain 6 characters");
+                    Log.v(TAG, "Password is must be at least contain 6 characters");
                     Toast.makeText(RegisterPage.this, "Password is must be at least contain 6 characters", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 if (confirmPassword.equals(""))//Chris - Check for empty Inputs
                 {
-                    Log.v(TAG,"Confirm Password");
+                    Log.v(TAG, "Confirm Password");
                     Toast.makeText(RegisterPage.this, "Confirm Password", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 if (!confirmPassword.equals(password))//Chris - To Confirm password
                 {
-                    Log.v(TAG,"Password Do Not Match");
+                    Log.v(TAG, "Password Do Not Match");
                     Toast.makeText(RegisterPage.this, "Password Do Not Match", Toast.LENGTH_SHORT).show();
                     return;
-                }
-                else{
+                } else {
                     progressBar.setVisibility(View.VISIBLE);//Chris - For user to know that the data is being processed
                     //Authentication
                     Auth.createUserWithEmailAndPassword(email, confirmPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -125,40 +130,53 @@ public class RegisterPage extends AppCompatActivity {
                                 progressBar.setVisibility(View.INVISIBLE);
                                 //Custom message if email is invaild
                                 Toast.makeText(RegisterPage.this, "The email is invaild", Toast.LENGTH_SHORT).show();
-                                Log.v(TAG,"The email is invaild");
-                            }
-
-                            else {
+                                Log.v(TAG, "The email is invaild");
+                            } else {
                                 progressBar.setVisibility(View.INVISIBLE);
 
                                 //Chris - Register is successful,saving user details to firebase database
                                 member.setName(EnterName.getText().toString());
                                 member.setEmail(EnterEmail.getText().toString());
                                 member.setPassword(EnterPassword.getText().toString());
-
                                 //Chris -  Customised user id,make sure no two users have the same user id
-                                String id= String.valueOf(maxid+1);
-
+                                String id = String.valueOf(maxid + 1);
                                 //Chris - Add the user to firebase database
                                 databaseReference.child(id).setValue(member);
+                                user = new User();
+                                user.setUsername(EnterName.getText().toString());
+                                user.setUserabout("");
+                                user.setUserisbn("");
+                                user.setUseridu(Integer.parseInt(id));
+                                user.setfollowingstring("");
+                                //Chris - Add the user to firebase database
+                                databaseReference.child(id).setValue(member);
+                                FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
 
-                                Log.v(TAG,"Registration Successfully");
-                                Toast.makeText(RegisterPage.this, "Registration Successfully", Toast.LENGTH_SHORT).show();
-
-                                //Go to login page after user successfully registered
-                                Intent login=new Intent(RegisterPage.this, LoginPage.class);
-                                startActivity(login);
-                                finish();
+                                final DocumentReference docRef = firebaseFirestore.collection("User").document(String.valueOf(user.getUseridu()));
+                                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                        if (task.isSuccessful()) {
+                                            Map<String, Object> data = new HashMap<>();
+                                            data.put("desc", user.getUserabout());
+                                            data.put("name", user.getUsername());
+                                            data.put("isbn", user.getUserisbn());
+                                            data.put("following", user.getfollowingstring());
+                                            docRef.set(data);
+                                            //Go to login page after user successfully registered
+                                            Intent login = new Intent(RegisterPage.this, LoginPage.class);
+                                            startActivity(login);
+                                            finish();
+                                        }
+                                    }
+                                });
                             }
                         }
-
-
                     });
 
                 }
+
             }
         });
-
     }
-
 }
